302d5bb50dc8e95ae383523c1ac6f5cab4d04db5
Muruts Gebretsadik <murutsmenge21@Murutss-MacBook-Air.local>
2024-12-05 12:53:27 +0000

    Initial commit with .gitignore to exclude node_modules and build

---
+++ src/components/Resume.jsx
@@ -0,0 +1,222 @@
+import React from "react";
+import resumeFile from "../documents/Muruts Gebretsadik CV.pdf";
+
+const Resume = ({ classicHeader, darkTheme }) => {
+  const educationDetails = [
+    {
+      yearRange: "2023 - 2024",
+      title: "Full Stack Software Development",
+      place: "University of College Dublin",
+      desc: "The Full Stack Software Developer program at University College Dublin focuses on building dynamic applications using Flask, Django, and React, equipping students with comprehensive skills in full-stack development for modern web solutions.",
+    },
+    {
+      yearRange: "2013 - 2018",
+      title: "Civil Engineering",
+      place: "Mekelle University",
+      desc: "The Civil Engineering program at Mekelle University covers design, sustainability, and programming with C++, alongside advanced tools like AutoCAD, SAP2000, and ETABS, preparing students for modern infrastructure challenges",
+    }
+   
+  ];
+
+  const experienceDetails = [
+    {
+      yearRange: "2022 - now",
+      title: "Jr. Web Developer",
+      place: "Upwork",
+      desc: "Worked as a Jr. Web Developer on Upwork, specializing in responsive web design and front-end development, delivering modern and user-friendly websites for various clients.",
+    },
+    {
+      yearRange: "2020 - 2022",
+      title: "Site Engineer",
+      place: "Tsegaye Birhane & His Family Real Estate",
+      desc: "Worked as a Site Engineer at Tsegaye Birhane & His Family Real Estate, overseeing the construction of apartments while ensuring quality standards, adherence to engineering practices, and timely project delivery."
+    },
+    {
+      YearRange: "2018 - 2020",
+      Title: "Office Engineer",
+      Place: "Jadia Construction Plc",
+      Description: "Worked as an Office Engineer at Jadia Construction Plc, focusing on the design and planning of highways, ensuring compliance with engineering standards and project requirements."
+    },
+  
+
+  ];
+
+  const skills = [
+    {
+      name: "Web Design",
+      percent: 65,
+    },
+    {
+      name: "HTML/CSS",
+      percent: 95,
+    },
+    {
+      name: "JavaScript",
+      percent: 80,
+    },
+    {
+      name: "React JS",
+      percent: 70,
+    },
+    {
+      name: "Python",
+      percent: 70,
+    },
+    {
+      name: "Bootstrap",
+      percent: 80,
+    },
+  ];
+
+  return (
+    <section
+      id="resume"
+      className={"section " + (darkTheme ? "bg-dark-1" : "")}
+    >
+      <div className={"container " + (classicHeader ? "" : "px-lg-5")}>
+        {/* Heading */}
+        <div className="position-relative d-flex text-center mb-5">
+          <h2
+            className={
+              "text-24  text-uppercase fw-600 w-100 mb-0 " +
+              (darkTheme ? "text-muted opacity-1" : "text-light opacity-4")
+            }
+          >
+            Summary
+          </h2>
+          <p
+            className={
+              "text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0 " +
+              (darkTheme ? "text-white" : "text-dark")
+            }
+          >
+            {" "}
+            Resume
+            <span className="heading-separator-line border-bottom border-3 border-primary d-block mx-auto" />
+          </p>
+        </div>
+        {/* Heading end*/}
+        <div className="row gx-5">
+          {/* My Education */}
+          <div className="col-md-6">
+            <h2
+              className={
+                "text-6 fw-600 mb-4 " + (darkTheme ? "text-white" : "")
+              }
+            >
+              My Education
+            </h2>
+            {educationDetails.length > 0 &&
+              educationDetails.map((value, index) => (
+                <div
+                  key={index}
+                  className={
+                    "bg-white  rounded p-4 mb-4 " +
+                    (darkTheme ? "bg-dark" : "bg-white border")
+                  }
+                >
+                  <p className="badge bg-primary text-2 fw-400">
+                    {value.yearRange}
+                  </p>
+                  <h3 className={"text-5 " + (darkTheme ? "text-white" : "")}>
+                    {value.title}
+                  </h3>
+                  <p className={darkTheme ? "text-primary" : "text-danger"}>
+                    {value.place}
+                  </p>
+                  <p className={"mb-0 " + (darkTheme ? "text-white-50" : "")}>
+                    {value.desc}
+                  </p>
+                </div>
+              ))}
+          </div>
+          {/* My Experience */}
+          <div className="col-md-6">
+            <h2
+              className={
+                "text-6 fw-600 mb-4 " + (darkTheme ? "text-white" : "")
+              }
+            >
+              My Experience
+            </h2>
+            {experienceDetails.length > 0 &&
+              experienceDetails.map((value, index) => (
+                <div
+                  key={index}
+                  className={
+                    "bg-white  rounded p-4 mb-4 " +
+                    (darkTheme ? "bg-dark" : "bg-white border")
+                  }
+                >
+                  <p className="badge bg-primary text-2 fw-400">
+                    {value.yearRange}
+                  </p>
+                  <h3 className={"text-5 " + (darkTheme ? "text-white" : "")}>
+                    {value.title}
+                  </h3>
+                  <p className={darkTheme ? "text-primary" : "text-danger"}>
+                    {value.place}
+                  </p>
+                  <p className={"mb-0 " + (darkTheme ? "text-white-50" : "")}>
+                    {value.desc}
+                  </p>
+                </div>
+              ))}
+          </div>
+        </div>
+        {/* My Skills */}
+        <h2
+          className={
+            "text-6 fw-600 mt-4 mb-4 " + (darkTheme ? "text-white" : "")
+          }
+        >
+          My Skills
+        </h2>
+        <div className="row gx-5">
+          {skills.length > 0 &&
+            skills.map((skill, index) => (
+              <div key={index} className="col-md-6">
+                <p
+                  className={
+                    " fw-500 text-start mb-2 " +
+                    (darkTheme ? "text-light" : "text-dark")
+                  }
+                >
+                  {skill.name}{" "}
+                  <span className="float-end">{skill.percent}%</span>
+                </p>
+                <div
+                  className={
+                    "progress progress-sm mb-4 " + (darkTheme ? "bg-dark" : "")
+                  }
+                >
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: skill.percent + "%" }}
+                    aria-valuenow={skill.percent}
+                    aria-valuemin={0}
+                    aria-valuemax={100}
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
+        <div className="text-center mt-5">
+          <a
+            className="btn btn-outline-secondary rounded-pill shadow-none"
+            href={resumeFile}
+            download
+          >
+            Download CV
+            <span className="ms-1">
+              <i className="fas fa-download" />
+            </span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
